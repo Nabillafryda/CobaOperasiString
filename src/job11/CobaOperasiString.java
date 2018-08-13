9dd352278baa88ae9c44cff1202cb1b0e49af72f
ASUS <ASUS@LAPTOP-B71SN63T>
2018-08-13 08:29:55 +0700

    Initial Commit

---
+++ src/job11/CobaOperasiString.java
@@ -0,0 +1,15 @@
+
+package job11;
+
+import java.util.Locale;
+
+public class CobaOperasiString {
+    public static void main(String[] args) {
+        
+        String identitas = "Nabilla Fryda Israhani Nugraha / XR3 / 28";
+        System.out.println("Identitas : " + identitas);
+        
+        
+
+    }    
+}
