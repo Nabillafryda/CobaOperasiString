86ba5f959819d359973ab35b6c3d0dc56611d27e
ASUS <ASUS@LAPTOP-B71SN63T>
2018-08-16 18:58:53 +0700

    Initial Commit

---
+++ src/job11/ConvertVocal2Angka.java
@@ -0,0 +1,13 @@
+package job11;
+
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");        
+    }
+
+
+}
+
