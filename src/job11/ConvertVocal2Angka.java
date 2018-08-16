c3193cdd676a609d364a239d72b2d0bcc5018159
ASUS <ASUS@LAPTOP-B71SN63T>
2018-08-16 19:36:32 +0700

    Tambah Konversi

--- src/job11/ConvertVocal2Angka.java
+++ src/job11/ConvertVocal2Angka.java
@@ -15,8 +15,22 @@ public class ConvertVocal2Angka {
         System.out.println("Kalimat Asli: " + kalimat);        
         return kalimat;
     }
+    private static String vocal2Angka(String kalimat){
+        char [][] arConvert = {{'a','4'}, {'i','1'}, {'u','2'}, {'e', '3'}, {'o', '0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i=0; i<arConvert.length; i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;  
+    } 
     public static void main(String[] args) {
-           String kalimat = tampilInput();
+           String kalimat = tampilInput();    
+           String convert = vocal2Angka(kalimat);
+    
+    
+    
+    
     }
 }
 
